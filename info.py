bda2e594fabe673b5d5697ebdcfb759bdb2ed7eb
Kanha01254 <kanhaiyalaljoshi804@gmail.com>
2024-11-21 22:22:49 +0530

    Update info.py

--- info.py
+++ info.py
@@ -21,12 +21,12 @@ BOT_TOKEN = environ.get('BOT_TOKEN', "7765742639:AAFH3_5znuV_dbNc4MQD8yzwm8bDsPD
 CACHE_TIME = int(environ.get('CACHE_TIME', 300))
 USE_CAPTION_FILTER = bool(environ.get('USE_CAPTION_FILTER', True))
 
-PICS = (environ.get('PICS', 'https://envs.sh/598.jpg')).split() #SAMPLE PIC
-NOR_IMG = environ.get("NOR_IMG", "https://envs.sh/598.jpg")
+PICS = (environ.get('PICS', 'https://envs.sh/GiI.jpg')).split() #SAMPLE PIC
+NOR_IMG = environ.get("NOR_IMG", "https://envs.sh/GiI.jpg")
 MELCOW_VID = environ.get("MELCOW_VID", "https://envs.sh/5rb.mp4")
-SPELL_IMG = environ.get("SPELL_IMG", "https://envs.sh/598.jpg")
-SUBSCRIPTION = (environ.get('SUBSCRIPTION', 'https://envs.sh/598.jpg'))
-CODE = (environ.get('CODE', 'https://envs.sh/598.jpg'))
+SPELL_IMG = environ.get("SPELL_IMG", "https://envs.sh/GiI.jpg")
+SUBSCRIPTION = (environ.get('SUBSCRIPTION', 'https://envs.sh/GiI.jpg'))
+CODE = (environ.get('CODE', 'https://envs.sh/GiI.jpg'))
 
 #stream link shortner
 STREAM_SITE = (environ.get('STREAM_SITE', 'api.shareus.io'))
