9952ed4aa074c748fe807e13a0dd865b9b206ab2
Kanha01254 <kanhaiyalaljoshi804@gmail.com>
2025-06-14 22:53:01 +0530

    Update info.py

--- info.py
+++ info.py
@@ -52,7 +52,7 @@ NO_RESULTS_MSG = bool(environ.get("NO_RESULTS_MSG", False)) # True if you want n
 # MongoDB information 
 # https://youtu.be/qFB0cFqiyOM?si=QGuFSZ7qhxl4VTrA
 
-DATABASE_URI = environ.get('DATABASE_URI=mongodb+srv://Kanha123:kanha12345@cluster0.qdynpm4.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
+DATABASE_URI = environ.get('DATABASE_URI = "mongodb+srv://Kanha123:kanha12345@cluster0.qdynpm4.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"")
 DATABASE_NAME = environ.get('DATABASE_NAME', "Cluster0")
 COLLECTION_NAME = environ.get('COLLECTION_NAME', 'Telegram_files')
 
